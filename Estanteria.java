bd765c989492e88958429e5cfd485c297fe332f3
ainarejos <ainarejos@nigul.cide.es>
2021-02-08 20:29:19 +0100

    Initial Push.

--- Estanteria.java
+++ Estanteria.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
-
 public class Estanteria {
     ArrayList<Llibre> estanteria = new ArrayList<>();
 
@@ -22,13 +21,38 @@ public class Estanteria {
         return Cl;
     }
 
-    public void EliminarLlibre(Llibre llibre){
-        Scanner sc = new Scanner(System.in);
-        String algo=sc.nextLine();
-        if (!ComprovaLlibre(llibre)&&(llibre.getTitol().equals(algo)||llibre.getAutor().equals(algo))){
-            estanteria.remove(llibre);
-            System.out.println("Ganamos");
+    public void EliminarLlibre(String llibre){
+        for (int i = 0; i < estanteria.size() ; i++) {
+            Llibre libro= estanteria.get(i);
+            if ((libro.getAutor().equals(estanteria.get(i).getAutor())||(libro.getTitol().equals(estanteria.get(i).getTitol())))){
+                estanteria.remove(i);
+                Llibre llibre0=new Llibre("", "", 0);
+                estanteria.add(llibre0);
+
+                System.out.println("Ganamos");
+            }
         }
+
+    }
+
+    private boolean ComprovarAutor(String llibre){
+        boolean Ca=false;
+        for (int i = 0; i <estanteria.size() ; i++) {
+            if (estanteria.get(i).getTitol().equals(llibre)){
+                Ca=true;
+            }
+        }
+        return Ca;
+    }
+
+    private boolean ComprovarTitulo(String llibre){
+        boolean Ca=false;
+        for (int i = 0; i <estanteria.size() ; i++) {
+            if (estanteria.get(i).getAutor().equals(llibre)){
+                Ca=true;
+            }
+        }
+        return Ca;
     }
 
     public void Top10(){
